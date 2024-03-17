ecbd78a6a08944033ec01961a84253d32ce2ad1a
warhogz <ds.odium69@gmail.com>
2024-03-18 01:24:53 +0300

    commit

---
+++ learning_log/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Включить URL авторизации по умолчанию.
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
+]
